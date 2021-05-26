b288cb522efa260091ca3f96e757f87ff0725c34
Szymon Kieroński <72864319+szmn-git@users.noreply.github.com>
2021-05-26 19:40:08 +0200

    init

---
+++ thingspeakSend.ps1
@@ -0,0 +1,17 @@
+﻿do{
+
+	$counter = $counter + 1
+	$temp = Get-Random -Minimum 15 -Maximum 90
+
+	Write-Host $temp.ToString()
+
+	#YOUR KEY
+	$key=""
+
+	$url="https://api.thingspeak.com/update?key=" + $key + "&field2=" + $temp
+
+	$result = Invoke-WebRequest -Uri $url
+
+	start-sleep -Seconds 60
+
+}while($counter -lt 30)
